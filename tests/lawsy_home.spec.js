c5cca10301ddfca0a160ad9280d572e38b19fff7
Anuj Kumar Karmakar <anujkarmakar999@gmail.com>
2023-10-05 07:42:43 +0530

    Test title and Nav bar

--- tests/lawsy_home.spec.js
+++ tests/lawsy_home.spec.js
@@ -3,6 +3,15 @@ const {test, expect} = require('@playwright/test');
 test('Test the title of Website', async({page}) => {
     await page.goto('https://anujk.co')
 
-    //Except the title to conatin Lawsy
-    await expect(page).toHaveTitle('Lawsy')
+    //Except the title to conatin a substring Lawsy
+    await expect(page).toHaveTitle(/Lawsy/)
 })
+
+test('Test the nav bar of the websites', async ({ page }) => {
+  await page.goto('https://anujk.co/');
+  await page.getByRole('link', { name: 'HOME' }).click();
+  await page.getByRole('link', { name: 'CONTENTS' }).click();
+  await page.getByRole('link', { name: 'ABOUT US' }).click();
+  await page.getByRole('link', { name: 'FAQs' }).click();
+  await page.getByRole('link', { name: 'HOME' }).click();
+});
